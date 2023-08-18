6db86afa462a09ac9be059baa939b64644978206
JUB_Yoush <yoush1337@gmail.com>
2023-08-17 21:50:32 -0400

    started 3

---
+++ code/variables/target/debug/deps/variables-af3b7adf1b067932.d
@@ -0,0 +1,5 @@
+/home/jaydenb/projects/rust/rust-notes/code/variables/target/debug/deps/variables-af3b7adf1b067932: src/main.rs
+
+/home/jaydenb/projects/rust/rust-notes/code/variables/target/debug/deps/variables-af3b7adf1b067932.d: src/main.rs
+
+src/main.rs:
---
+++ code/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/jaydenb/projects/rust/rust-notes/code/variables/target/debug/variables: /home/jaydenb/projects/rust/rust-notes/code/variables/src/main.rs
